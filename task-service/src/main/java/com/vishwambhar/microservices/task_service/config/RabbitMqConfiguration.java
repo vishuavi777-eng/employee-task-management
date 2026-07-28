da641ffd095433ab33aa9b57bc714aec3c32be51
Vishwambhar <panda@Vishwambhars-MacBook-Air.local>
2026-07-28 22:50:58 +0530

    rabbit-mq: configuration first stage done.

---
+++ task-service/src/main/java/com/vishwambhar/microservices/task_service/config/RabbitMqConfiguration.java
@@ -0,0 +1,41 @@
+package com.vishwambhar.microservices.task_service.config;
+
+import com.vishwambhar.microservices.task_service.messaging.RabbitMqNames;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfiguration {
+
+    @Bean
+    public TopicExchange taskEventsExchange() {
+        return new TopicExchange(
+                RabbitMqNames.TASK_EVENTS_EXCHANGE,
+                true,
+                false
+        );
+    }
+
+    @Bean
+    public Queue taskCreatedQueue() {
+        return QueueBuilder
+                .durable(RabbitMqNames.TASK_CREATED_QUEUE)
+                .build();
+    }
+
+    @Bean
+    public Binding taskCreatedBinding(
+            Queue taskCreatedQueue,
+            TopicExchange taskEventsExchange
+    ) {
+        return BindingBuilder
+                .bind(taskCreatedQueue)
+                .to(taskEventsExchange)
+                .with(RabbitMqNames.TASK_CREATED_ROUTING_KEY);
+    }
+}
