4a558801dae51ff32526d723b294482f5b587871
Vishwambhar <panda@Vishwambhars-MacBook-Air.local>
2026-07-29 22:58:19 +0530

    (task-service):test enviorment setup problem solved for (./mvnw clean package)

--- task-service/src/test/java/com/vishwambhar/microservices/task_service/TaskServiceApplicationTests.java
+++ task-service/src/test/java/com/vishwambhar/microservices/task_service/TaskServiceApplicationTests.java
@@ -2,8 +2,17 @@ package com.vishwambhar.microservices.task_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@SpringBootTest(args = {
+		"--spring.cloud.config.fail-fast=false",
+		"--spring.cloud.discovery.enabled=false",
+		"--eureka.client.enabled=false",
+		"--eureka.client.register-with-eureka=false",
+		"--eureka.client.fetch-registry=false"
+})
 class TaskServiceApplicationTests {
 
 	@Test
