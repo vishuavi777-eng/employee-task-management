3c5b494a4a078fdc238a70c32aa4d521bee119cd
Vishwambhar <panda@Vishwambhars-MacBook-Air.local>
2026-07-29 23:18:50 +0530

    (notification-service):add testing enviorment setup. not package command work

--- notification-service/src/test/java/com/vishwambhar/microservices/notification_service/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/vishwambhar/microservices/notification_service/NotificationServiceApplicationTests.java
@@ -2,8 +2,17 @@ package com.vishwambhar.microservices.notification_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@SpringBootTest(args = {
+		"--spring.config.import=",
+		"--spring.cloud.config.fail-fast=false",
+		"--spring.cloud.discovery.enabled=false",
+		"--eureka.client.enabled=false",
+		"--eureka.client.register-with-eureka=false",
+		"--eureka.client.fetch-registry=false"
+})
 class NotificationServiceApplicationTests {
 
 	@Test
