f167cf15ffb231ffd1e7cbbee552603969e1f3d1
Vishwambhar <panda@Vishwambhars-MacBook-Air.local>
2026-07-29 20:47:31 +0530

    Fix employee-service test datasource for package build

--- employee-service/src/test/java/com/vishwambhar/microservices/employee_service/EmployeeServiceApplicationTests.java
+++ employee-service/src/test/java/com/vishwambhar/microservices/employee_service/EmployeeServiceApplicationTests.java
@@ -2,8 +2,16 @@ package com.vishwambhar.microservices.employee_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@SpringBootTest(args = {
+		"--spring.cloud.config.fail-fast=false",
+		"--spring.cloud.discovery.enabled=false",
+		"--eureka.client.enabled=false",
+		"--eureka.client.register-with-eureka=false",
+		"--eureka.client.fetch-registry=false"
+})
 class EmployeeServiceApplicationTests {
 
 	@Test
