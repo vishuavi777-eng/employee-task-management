15eb446b64641f759891145d71ab177f9541b3e1
Vishwambhar <panda@Vishwambhars-MacBook-Air.local>
2026-07-29 23:27:06 +0530

    (api-gateway):testing envirment setup done

--- api-gateway/src/test/java/com/vishwambhar/microservices/api_gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/vishwambhar/microservices/api_gateway/ApiGatewayApplicationTests.java
@@ -2,7 +2,9 @@ package com.vishwambhar.microservices.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
